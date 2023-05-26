b5f31b29a7e3203f47553b9991c9374dbb8a4d14
Angiegit config --global user.name 'Angie <adeschutter6437@gmail.com>
2023-05-25 20:43:39 -0700

    used different styling techniques

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
 import './App.css';
+import spalshImage from './embedded/background.webp'
+import TopBar from './components/TopBar';
+import styled from'styled-components'
+import StoreLanding from './components/StoreLanding';
+
+const Splash = styled.div`
+  width:100vw;
+  height:250px;
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-image: url(${spalshImage});
+  background-color: #dfeed6;
+  position: absolute;
+  left:0;
+  top: 40px;
+  right: 0;
+  z-index: -1;
+`
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
+  return (<>
+  <TopBar />  
+  <Splash />
+  <StoreLanding />
+  </>
   );
 }
 
---
+++ src/components/TopBar.jsx
@@ -0,0 +1,34 @@
+import instcartlogo from '../embedded/logo.svg'
+import { Navbar, Button, Nav } from 'react-bootstrap'
+
+export default function TopBar() {
+    return <Navbar 
+                bg="light" 
+                expand="lg" 
+                fixed='top'
+            >
+        <Navbar.Brand
+            style={
+                {
+                    margin: "5px 0 0 20px"
+                }
+            }
+            href="#home"
+        >
+            <img
+                src={instcartlogo}
+                alt='instacart logo'
+            />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="mr-auto">
+            </Nav>
+            <Nav>
+                <Nav.Link href="#LogIn">Log In</Nav.Link>
+                <Button variant="success">Sign Up</Button>{' '}
+            </Nav>
+        </Navbar.Collapse>
+    </Navbar>
+    
+}
